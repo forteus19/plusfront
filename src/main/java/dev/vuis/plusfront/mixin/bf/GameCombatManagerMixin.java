316f878b4b30aeef2ccd500241bb6a5ac672288c
forteus19 <jaxonpowe@gmail.com>
2026-07-12 14:00:56 -0400

    stupid combat manager

--- src/main/java/dev/vuis/plusfront/mixin/bf/GameCombatManagerMixin.java
+++ src/main/java/dev/vuis/plusfront/mixin/bf/GameCombatManagerMixin.java
@@ -4,16 +4,38 @@ import com.boehmod.blockfront.common.BFAbstractManager;
 import com.boehmod.blockfront.common.stat.BFStat;
 import com.boehmod.blockfront.game.AbstractGame;
 import com.boehmod.blockfront.game.GameCombatManager;
-import com.boehmod.blockfront.game.GameStatus;
 import com.boehmod.blockfront.game.GameUtils;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import dev.vuis.plusfront.util.PFUtil;
 import java.util.UUID;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameCombatManager.class)
 public abstract class GameCombatManagerMixin {
+	@Shadow
+	@Final
+	private @NotNull AbstractGame<?, ?, ?> game;
+
+	@Definition(id = "GAME", field = "Lcom/boehmod/blockfront/game/GameStatus;GAME:Lcom/boehmod/blockfront/game/GameStatus;")
+	@Expression("? == GAME")
+	@ModifyExpressionValue(
+		method = "method_2808",
+		at = @At(
+			value = "MIXINEXTRAS:EXPRESSION",
+			ordinal = 0
+		)
+	)
+	private boolean checkStatusForStats(boolean original) {
+		return PFUtil.allowStatChanges(game);
+	}
+
 	@Redirect(
 		method = "addKillFeedEntry",
 		at = @At(
@@ -21,8 +43,8 @@ public abstract class GameCombatManagerMixin {
 			target = "Lcom/boehmod/blockfront/game/GameUtils;incrementPlayerStat(Lcom/boehmod/blockfront/common/BFAbstractManager;Lcom/boehmod/blockfront/game/AbstractGame;Ljava/util/UUID;Lcom/boehmod/blockfront/common/stat/BFStat;)V"
 		)
 	)
-	private void onlyCountStatDuringGame(BFAbstractManager<?, ?, ?> manager, @NotNull AbstractGame<?, ?, ?> game, @NotNull UUID player, @NotNull BFStat stat) {
-		if (game.getStatus() == GameStatus.GAME) {
+	private void checkKfeMethodStats(BFAbstractManager<?, ?, ?> manager, @NotNull AbstractGame<?, ?, ?> game, @NotNull UUID player, @NotNull BFStat stat) {
+		if (PFUtil.allowStatChanges(game)) {
 			GameUtils.incrementPlayerStat(manager, game, player, stat);
 		}
 	}
--- src/main/java/dev/vuis/plusfront/util/PFUtil.java
+++ src/main/java/dev/vuis/plusfront/util/PFUtil.java
@@ -5,10 +5,12 @@ import com.boehmod.blockfront.common.BFAbstractManager;
 import com.boehmod.blockfront.common.player.BFAbstractPlayerData;
 import com.boehmod.blockfront.common.player.PlayerDataHandler;
 import com.boehmod.blockfront.game.AbstractGame;
+import com.boehmod.blockfront.game.GameStatus;
 import com.boehmod.blockfront.game.GameTeam;
 import com.boehmod.blockfront.game.GameUtils;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import dev.vuis.plusfront.PlusFront;
+import dev.vuis.plusfront.game.impl.def.DefusalGame;
 import dev.vuis.plusfront.net.payload.PFFeatureFlagsPayload;
 import dev.vuis.plusfront.world.PFSavedData;
 import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
@@ -24,6 +26,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.network.PacketDistributor;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public final class PFUtil {
@@ -88,6 +91,13 @@ public final class PFUtil {
 
 	public static SuggestionProvider<CommandSourceStack> suggestGames() {
 		return (context, builder) -> SharedSuggestionProvider.suggest(blockfrontManager().getGames().keySet(), builder);
+	}
+
+	public static boolean allowStatChanges(@NotNull AbstractGame<?, ?, ?> game) {
+		return switch (game) {
+			case DefusalGame defusalGame -> defusalGame.isRoundInProgress();
+			default -> game.getStatus() == GameStatus.GAME;
+		};
 	}
 
 	public static void updateFeatureFlags(Map<String, Boolean> featureFlags) {
